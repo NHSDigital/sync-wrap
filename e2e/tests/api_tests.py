0a462e08b46d683ea637cb50f67f58af2a0077b2
matt.mercer <matt.mercer@nhs.net>
2020-12-30 14:19:41 +0000

    apm-1351: add smoke tests for beyond dev

--- e2e/tests/api_tests.py
+++ e2e/tests/api_tests.py
@@ -5,6 +5,7 @@ from api_test_utils.api_session_client import APISessionClient
 from api_test_utils.api_test_session_config import APITestSessionConfig
 
 
+@pytest.mark.smoketest
 @pytest.mark.asyncio
 async def test_wait_for_ping(api_client: APISessionClient, api_test_config: APITestSessionConfig):
 
@@ -22,6 +23,7 @@ async def test_wait_for_ping(api_client: APISessionClient, api_test_config: APIT
     )
 
 
+@pytest.mark.smoketest
 @pytest.mark.asyncio
 async def test_wait_for_status(api_client: APISessionClient, api_test_config: APITestSessionConfig):
 
@@ -43,6 +45,7 @@ async def test_wait_for_status(api_client: APISessionClient, api_test_config: AP
     )
 
 
+@pytest.mark.smoketest
 @pytest.mark.asyncio
 async def test_api_status_with_service_header_another_service(api_client: APISessionClient):
 
@@ -53,6 +56,7 @@ async def test_api_status_with_service_header_another_service(api_client: APISes
         assert body.get('service') == 'sync-wrap'
 
 
+@pytest.mark.smoketest
 @pytest.mark.asyncio
 async def test_api_status_with_service_header(api_client: APISessionClient):
 
