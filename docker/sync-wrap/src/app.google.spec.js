0e43c5b7893eb9d0cef704f23d377143f5aa2479
Sathiyakumar Ganesan <sathiyakumar.ganesan1@nhs.net>
2026-03-12 12:31:43 +0000

    debug

--- docker/sync-wrap/src/app.google.spec.js
+++ docker/sync-wrap/src/app.google.spec.js
@@ -51,32 +51,33 @@ describe("express with google upstream and botli decompress", function () {
             .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
             .set("Accept-Encoding", "gzip, deflate, br")
             .set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
-            // .expect(r => {
-            //     expect(r.text).to.be.a("string").and.satisfy(body => body.startsWith("<!doctype html>"));
-            //     assert.equal(r.headers["content-encoding"], "gzip");
-            // })
-            // .expect(200, done);
-            .
-            expect((res) => {
-            // Status
-            // eslint-disable-next-line no-console
-            console.log("DEBUG status:", res.status);
-
-            // Headers
-            // eslint-disable-next-line no-console
-            console.log("DEBUG headers:", res.headers);
-
-            // Body (string for HTML; res.body is used for JSON)
-            const snippet = (res.text || "").slice(0, 500);
-            // eslint-disable-next-line no-console
-            console.log("DEBUG body first 500 chars:\n", snippet);
+            .expect(r => {
+                expect(r.text).to.be.a("string").and.satisfy(body => body.startsWith("<!doctype html>"));
+                assert.equal(r.headers["content-encoding"], "gzip");
             })
             .expect(200, done);
+            
+            // .expect((res) => {
+            // // Status
+            // // eslint-disable-next-line no-console
+            // console.log("DEBUG status:", res.status);
+
+            // // Headers
+            // // eslint-disable-next-line no-console
+            // console.log("DEBUG headers:", res.headers);
+
+            // // Body (string for HTML; res.body is used for JSON)
+            // const snippet = (res.text || "").slice(0, 500);
+            // // eslint-disable-next-line no-console
+            // console.log("DEBUG body first 500 chars:\n", snippet);
+            // })
+            // .expect(200, done);
     });
 
     it("returns good content for / with chrome headers", (done) => {
         request(server)
             .get("/")
+            .redirects(1)
             .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
             .set("Accept-Encoding", "gzip, deflate, br")
             .set("Cache-Control", "no-cache")
@@ -127,6 +128,7 @@ describe("express with google upstream without botli decompress", function () {
     it("returns content-encoding br / with accept-encoding and User-Agent", (done) => {
         request(server)
             .get("/")
+            .redirects(1)
             .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
             .set("Accept-Encoding", "gzip, deflate, br")
             .set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
