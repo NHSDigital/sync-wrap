5bb3c4f8689ca24cb13b22500ad41ab2901c7f03
Sathiyakumar Ganesan <sathiyakumar.ganesan1@nhs.net>
2026-03-12 13:22:58 +0000

    debug

--- docker/sync-wrap/src/app.google.spec.js
+++ docker/sync-wrap/src/app.google.spec.js
@@ -19,7 +19,7 @@ describe("express with google upstream and botli decompress", function () {
         env = process.env;
         let app = require("./app");
         app.setup({
-            UPSTREAM: "www.google.co.uk",
+            UPSTREAM: "https://www.google.co.uk",
             LOG_LEVEL: "DEBUG",
             UNBOTLI: "true"
         });
@@ -47,6 +47,19 @@ describe("express with google upstream and botli decompress", function () {
     it("returns good content for / with accept-encoding and User-Agent", (done) => {
         request(server)
             .get("/")
+            .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+            .set("Accept-Encoding", "gzip, deflate, br")
+            .set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
+            .expect(r => {
+                expect(r.text).to.be.a("string").and.satisfy(body => body.startsWith("<!doctype html>"));
+                assert.equal(r.headers["content-encoding"], "gzip");
+            })
+            .expect(200, done);
+    });
+
+    it("[DEBUG] returns good content for / with accept-encoding and User-Agent", (done) => {
+        request(server)
+            .get("/")
             .redirects(1)
             .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
             .set("Accept-Encoding", "gzip, deflate, br")
@@ -69,7 +82,27 @@ describe("express with google upstream and botli decompress", function () {
     it("returns good content for / with chrome headers", (done) => {
         request(server)
             .get("/")
-            .redirects(1)
+            .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+            .set("Accept-Encoding", "gzip, deflate, br")
+            .set("Cache-Control", "no-cache")
+            .set("Connection", "keep-alive")
+            .set("Pragma", "no-cache")
+            .set("Sec-Fetch-Dest", "document")
+            .set("Sec-Fetch-Mode", "navigate")
+            .set("Sec-Fetch-Site", "none")
+            .set("Sec-Fetch-User", "?1")
+            .set("Upgrade-Insecure-Requests", "1")
+            .set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
+            .expect(r => {
+                expect(r.text).to.be.a("string").and.satisfy(body => body.startsWith("<!doctype html>"));
+                assert.equal(r.headers["content-encoding"], "gzip");
+            })
+            .expect(200, done);
+    });
+
+    it("[DEBUG] returns good content for / with chrome headers", (done) => {
+        request(server)
+            .get("/")
             .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
             .set("Accept-Encoding", "gzip, deflate, br")
             .set("Cache-Control", "no-cache")
@@ -127,7 +160,6 @@ describe("express with google upstream without botli decompress", function () {
     it("returns content-encoding br / with accept-encoding and User-Agent", (done) => {
         request(server)
             .get("/")
-            .redirects(1)
             .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
             .set("Accept-Encoding", "gzip, deflate, br")
             .set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
